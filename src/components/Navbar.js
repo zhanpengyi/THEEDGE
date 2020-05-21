dad8c38122599b8e0bafcf75a44bc353282e8150
zhanpeng yi <solution5678yi@gmail.com>
2020-05-20 21:10:14 -0700

    my first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/logo.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -49,6 +49,9 @@ const Navbar = class extends React.Component {
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
               data-target="navMenu"
               onClick={() => this.toggleHamburger()}
+              onKeyDown={() => this.toggleHamburger()}
+              role = 'button'
+              tabIndex = '0'
             >
               <span />
               <span />
